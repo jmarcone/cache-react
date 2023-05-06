64656619f3d0f7634cfa68564cde7ca8db159a19
jmarcone <marconi.julian@gmail.com>
2023-05-06 11:34:36 +0200

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const remoteUrlRef = useRef();
+  const cacheKeyRef = useRef();
+  const [result, setResult] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const Log = ({ value, replacer = null, space = 2 }) => (
+    <pre>
+      <code>{JSON.stringify(value, replacer, space)}</code>
+    </pre>
+  )
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const { data } = await axios.post("http://localhost:8000/remote-cache", {
+      "remoteUrl": remoteUrlRef.current.value,
+      "cacheKey": cacheKeyRef.current.value,
+    })
+    setLoading(false);
+    setResult(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        Url
+        <input type="text" name="remote-url" ref={remoteUrlRef} />
+        <br />
+        Cache Key
+        <input type="text" name="cacheKey" ref={cacheKeyRef} />
+        <button type='submit' >Send</button>
+      </form>
+        {
+          loading && "Loading....."
+        }
+        {
+
+          !loading && result && result.map((item, index) => (
+            <pre key={index}>
+              <code>{JSON.stringify(item, "", 2)}</code>
+            </pre>
+          ))
+
+        }
+      
     </div>
   );
 }
